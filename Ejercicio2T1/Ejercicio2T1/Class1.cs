2d6aedc17a7c41fb0704284e6491575fc1077714
Mardel Lara <alexanderchandy2018@gmail.com>
2023-03-03 12:09:56 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio2T1/Ejercicio2T1/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio2T1
+{
+    public class Class1
+    {
+        public int id { get; set; }
+        public int numero1 { get; set; }
+        public int numero2 { get; set; }
+        public double Resultado { get; set; }
+    }
+}
---
+++ Ejercicio2T1/Ejercicio2T1/MainPage.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio2T1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnsuma_Clicked(object sender, EventArgs e)
+        {
+
+            var datos = new Class1
+            {
+
+                Resultado = int.Parse(txtn1.Text) + int.Parse(txtn2.Text),
+
+            };
+
+            var secondpage = new Pagemostrar();
+            secondpage.BindingContext = datos;
+            Navigation.PushAsync(secondpage);
+
+        }
+
+        private void btnresta_Clicked(object sender, EventArgs e)
+        {
+            var datos = new Class1
+            {
+
+                Resultado = int.Parse(txtn1.Text) - int.Parse(txtn2.Text),
+
+            };
+
+            var secondpage = new Pagemostrar();
+            secondpage.BindingContext = datos;
+            Navigation.PushAsync(secondpage);
+
+        }
+
+        private void btndividir_Clicked(object sender, EventArgs e)
+        {
+            var datos = new Class1
+            {
+
+                Resultado = double.Parse(txtn1.Text) / double.Parse(txtn2.Text),
+
+            };
+
+            var secondpage = new Pagemostrar();
+            secondpage.BindingContext = datos;
+            Navigation.PushAsync(secondpage);
+
+        }
+
+        private void btnmultiplicar_Clicked(object sender, EventArgs e)
+        {
+            var datos = new Class1
+            {
+
+                Resultado = int.Parse(txtn1.Text) * int.Parse(txtn2.Text),
+
+            };
+
+            var secondpage = new Pagemostrar();
+            secondpage.BindingContext = datos;
+            Navigation.PushAsync(secondpage);
+
+        }
+
+
+    }
+}
